21cbae583c04d499aba4168647037b4cfa936ad2
German G Sayago <german.sayago@gmail.com>
2020-08-28 21:00:28 -0300

    se agrego axios al formulario y se implemento el envio

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,111 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function Form(props) {
-  return (
-    <div className="form">
-      <form>
-        <div className="card">
-          <div className="card-body">
-            <h5>Completa tus datos aquí</h5>
-            <div className="form-group">
-              <label htmlFor="">Nombre</label>
-              <input type="text" className="form-control" />
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      phone: "",
+      isSending: false,
+      isSended: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      isSending: true,
+    });
+
+    axios.post("http://sieteuno.com.ar/api-landing/api").then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        this.setState({
+          fname: "",
+          lname: "",
+          email: "",
+          phone: "",
+          isSending: false,
+          isSended: true,
+        });
+        window.location.assign(process.env.PUBLIC_URL + "/gracias");
+      }
+    });
+  }
+
+  render() {
+    const isSending = this.state.isSending;
+    return (
+      <div className="form">
+        <form onSubmit={this.handleSubmit}>
+          <div className="card">
+            <div className="card-body">
+              <h5>Completa tus datos aquí</h5>
+              <div className="form-group">
+                <label htmlFor="">Nombre</label>
+                <input
+                  type="text"
+                  name="fname"
+                  className="form-control"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="">Apellido</label>
+                <input
+                  type="text"
+                  name="lname"
+                  className="form-control"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="">Teléfono</label>
+                <input
+                  type="text"
+                  name="phone"
+                  className="form-control"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group mb-4">
+                <label htmlFor="">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  className="form-control"
+                  onChange={this.handleChange}
+                />
+              </div>
+              {/* <div className="btn btn-primary btn-lg btn-block">REGISTRATE</div> */}
+              {/* <a href="/gracias" className="btn btn-primary btn-lg btn-block">
+                REGISTRATE
+              </a> */}
+              <button
+                className="btn btn-primary btn-lg btn-block"
+                type="submit"
+                disabled={isSending}
+              >
+                {isSending ? "ENVIANDO ..." : "REGISTRAME"}
+              </button>
             </div>
-            <div className="form-group">
-              <label htmlFor="">Apellido</label>
-              <input type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="">Teléfono</label>
-              <input type="text" className="form-control" />
-            </div>
-            <div className="form-group mb-4">
-              <label htmlFor="">Email</label>
-              <input type="text" className="form-control" />
-            </div>
-            {/* <div className="btn btn-primary btn-lg btn-block">REGISTRATE</div> */}
-            <a href="/gracias" className="btn btn-primary btn-lg btn-block">
-              REGISTRATE
-            </a>
           </div>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
