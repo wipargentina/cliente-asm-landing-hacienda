90f0c87dba5bb36c429a5aed74381cbb8d29ed71
German G Sayago <german.sayago@gmail.com>
2020-09-01 21:35:19 -0300

    se agregaron botones de whatsapp en pagina de agradecimiento

--- src/pages/Tanks.js
+++ src/pages/Tanks.js
@@ -17,9 +17,38 @@ function Tanks(props) {
             </p>
             <h4 className="mt-3 mb-3">Escribinos por Whatsapp</h4>
             <div className="whatsapp">
-              <a href="https://wa.me/543516509975" className="btn btn-success">
-                <img src={faWhatsapp} alt="" /> +54 351 650 9975
-              </a>
+              <div className="row justify-content-center">
+                <div className="col-md-3">
+                  <h5>Hacienda Gorda</h5>
+                  <h6>Francisco Garin</h6>
+                  <a
+                    href="https://wa.me/543516509975"
+                    className="btn btn-success"
+                  >
+                    <img src={faWhatsapp} alt="" /> +54 351 650 9975
+                  </a>
+                </div>
+                <div className="col-md-3">
+                  <h5>Invernada y Cría</h5>
+                  <h6>Mario Masanti</h6>
+                  <a
+                    href="https://wa.me/3583647964"
+                    className="btn btn-success"
+                  >
+                    <img src={faWhatsapp} alt="" /> +54 358 3647 964
+                  </a>
+                </div>
+                <div className="col-md-3">
+                  <h5>Reproductores</h5>
+                  <h6>Leonardo Pérez</h6>
+                  <a
+                    href="https://wa.me/543583640202"
+                    className="btn btn-success"
+                  >
+                    <img src={faWhatsapp} alt="" /> +54 358 3640 202
+                  </a>
+                </div>
+              </div>
             </div>
             <h4 className="mb-2">Te invitamos a visites nuestra web</h4>
             <a
