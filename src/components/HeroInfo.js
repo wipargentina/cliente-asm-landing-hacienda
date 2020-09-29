fab3075735386a127d966c99f0172ac7b272af45
German G Sayago <german.sayago@gmail.com>
2020-09-29 20:21:42 -0300

    comillas

--- src/components/HeroInfo.js
+++ src/components/HeroInfo.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import logoCanalRural from '../assets/static/logo-canal-rural.png'
-import logoDonBenjamin from '../assets/static/logo-don-benjamin.png'
+import React from 'react';
+import logoCanalRural from '../assets/static/logo-canal-rural.png';
+import logoDonBenjamin from '../assets/static/logo-don-benjamin.png';
 
 function HeroInfo(props) {
   return (
-    <div className="info">
-      <div className="row mb-3">
-        <div className="col">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-title">Inicio del Remate</div>
-              <div className="card-date">
+    <div className='info'>
+      <div className='row mb-3'>
+        <div className='col'>
+          <div className='card'>
+            <div className='card-body'>
+              <div className='card-title'>Inicio del Remate</div>
+              <div className='card-date'>
                 Viernes 25 de Septiembre <br /> 13.30 horas
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-title">Estancias y Cabaña</div>
-              <div className="card-logo">
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='card'>
+            <div className='card-body'>
+              <div className='card-title'>Estancias y Cabaña</div>
+              <div className='card-logo'>
                 <img
                   src={logoDonBenjamin}
-                  alt="canal rural"
-                  className="logo-don-benjamin"
+                  alt='canal rural'
+                  className='logo-don-benjamin'
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <div className="card-title">En Vivo por</div>
-              <div className="card-logo">
+        <div className='col-md-4'>
+          <div className='card'>
+            <div className='card-body'>
+              <div className='card-title'>En Vivo por</div>
+              <div className='card-logo'>
                 <img
                   src={logoCanalRural}
-                  alt="canal rural"
-                  className="logo-canal-rural"
+                  alt='canal rural'
+                  className='logo-canal-rural'
                 />
               </div>
             </div>
@@ -48,7 +48,7 @@ function HeroInfo(props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HeroInfo
+export default HeroInfo;
