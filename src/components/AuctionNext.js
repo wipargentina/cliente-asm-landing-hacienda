f8bb3dbaba4573e1a3c6161830a1cb5df14f8984
German G Sayago <german.sayago@gmail.com>
2020-11-22 10:30:53 -0300

    se quito boton de preofertas y se agrego texto

--- src/components/AuctionNext.js
+++ src/components/AuctionNext.js
@@ -10,19 +10,13 @@ export default function AuctionNext(props) {
         {moment(item.fecha).format('dddd D')} de{' '}
         {moment(item.fecha).format('MMMM')}, {moment(item.fecha).format('LT')}hs
         {item.televisado && <span> por Canal Rural</span>}
-        {item.link_preoferta && (
-          <div className='mt-2'>
-            <a
-              className='btn btn-dark'
-              target='_blank'
-              rel='noopener noreferrer'
-              href={item.link_preoferta}
-            >
-              Acceso a Preoferta
-            </a>
-          </div>
-        )}
       </h4>
+      {item.link_preoferta && (
+        <div className='mt-2'>
+          <b>Pre oferta disponible</b>, complete el formulario para obtener el
+          enlace de acceso.
+        </div>
+      )}
     </div>
   );
 }
